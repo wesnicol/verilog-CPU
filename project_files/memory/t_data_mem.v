8a1e03806e2bb29944101da7f440b3930a9c28ef
Wes Nicol <38409659+wesnicol@users.noreply.github.com>
2019-04-02 18:33:37 -0500

    added data memory module with testbench

--- project_files/memory/t_data_mem.v
+++ project_files/memory/t_data_mem.v
@@ -6,31 +6,34 @@ Date  : 2019-03-21
 Class : HDL
 Professor: Mark Welker 
 
-Module: t_memory (test bench)
+Module: t_data_memory (test bench)
 	
 Purpose: Test Bench for the data memory module. This test bench provides a clock, an index,
          sample data to write to memory, and a bit dictating whether to read or write
 
 Expected Result: if write bit = 1, data will be written to addressed spot in memory
-                 if write bit = 0, data will be read from addressed spot in memory
+                 if read bit = 1, data will be read from addressed spot in memory
 
 
 *****************************************************************/
 
 `timescale 1ns / 1ns
-module t_memory;
+module t_data_memory;
+
+integer i, j, n; // used as index in while loops
 
 // test bench generates & supplies these values to module
-reg write_data, clk;
-reg [25:0] data_to_write;
-reg [3:0] prog_pointer;
+reg write_data, read_data, reset, clk;
+reg [255:0] data_to_write;
+reg [2:0] pointer;
+
 
 // test bench monitors these values (outputs of module being tested)
-wire [25:0] opcode;
+wire [255:0] data;
 
 
 
-instr_mem foo(opcode, prog_pointer, write_data, data_to_write, clk);
+data_mem foo(data, pointer, write_data, read_data, data_to_write, reset, clk);
 
 initial // Clock generator
   begin
@@ -39,49 +42,94 @@ initial // Clock generator
   end
 
 
-/****************************
+/*********************
 initial	// Reset test
   begin
     reset = 0;
     #5 reset = 1;
     #4 reset = 0;
   end
-****************************/
+*********************/
 
 
- 
-initial // set up initial conditions
-  begin
-	prog_pointer = 0; // set pointer to first index
+initial // set flags to zero
+  begin 
+	write_data = 0;
+	read_data = 0;
+	reset = 0;
   end
-
  
-always @ (posedge clk) // write/read each spot in memory
+initial // write/read each spot in memory
   begin
 
+/***************************************
+WRITE AND READ TO EVERY SPOT IN MEMORY
+***************************************/
+    i = 0;
+    while(i < 6) // 6 spots to wirte to in memory
+	  begin
 
-	// write 5555h to spot in memory
-	data_to_write = 26'h5555555; // write 5555 (hexidecimal)
-	write_data = 1; // indicate a write should be executed
+	    pointer = i; // write 5555h to spot j in memory
+		data_to_write = 256'h5555555555555555555555555555555555555555555555555555555555555555; // write 5555 to all 256 bits (hexidecimal)
+		#1 write_data = 1; // indicate a write should be executed
+		#1 write_data = 0;
 	
 	// read previous spot in memory (should be 5555h)
-	#1 write_data = 0; // indicate a read should be executed
-	
-	
+		#1 read_data = 1;
+		#1 read_data = 0;
 	
 	// write AAAAh to spot in memory
-	
-	#1 data_to_write = 26'hAAAAAAA; // write AAAA (hexidecimal)
-	#1 write_data = 1; // indicate a write should be executed
+		#1 data_to_write = 256'hAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA; // write AAAA (hexidecimal)
+		#1 write_data = 1; // indicate a write should be executed
+		#1 write_data = 0;
 	
 	// read previous spot in memory
-	#1 write_data = 0;
-	
-	#9 prog_pointer <= prog_pointer + 1; // add 1 to prog_pointer to advance thorugh memory
-	
-  end
+		#1 read_data = 1;
+		#1 read_data = 0;
+		
+		
+/********************************************************
+READ FROM OTHER SPOTS IN MEMORY TO SHOW THEY ARE EMPTY
+********************************************************/
+        j = 0;
+        while(j < 6)
+		  begin 
+		    pointer = j;
+			#1 read_data = 1; // read spot in memory at loc n
+			#1 read_data = 0;
+			j = j+1;
+		  end // end while loop with index j 
+		
+		pointer = i; // reassign the pointer to the current index
+		
+		
+		
+/***************************************
+WALK AA ACROSS MEMORY
+***************************************/
+		#1 data_to_write = 256'hAA; // begin with 000...00AA
+		
+		
+		
+		n = 0; // start index from 0
+		while(n < 32) // walking AA across all spots
+		  begin
+			#1 write_data = 1;
+			#1 write_data = 0;
+		
+			#1 read_data = 1;
+			#1 read_data = 0;
+		
+			#1 data_to_write = data_to_write * 256'h100; // this multiplication shifts AA two places left
+		  
+			n = n+1; // update index
+		  end // end while loop with index n
+	  
+	    i = i+1;
+	  end // end while loop with index i
+  end // end always block
 
-initial  // monoitor outpus here
-    $monitor($stime, opcode);
+initial  // monoitor outputs here
+    $monitor($stime, data);
 
 endmodule
